95bf43d3af703b3c6cba09218cfeca0972e1e618
Carl Johnsen <carljohannesjohnsen@yahoo.dk>
2013-07-01 13:42:17 +0200

    Added selector to the settings/delete buttons

--- MeetingBooker/gen/com/desc/meetingbooker/R.java
+++ MeetingBooker/gen/com/desc/meetingbooker/R.java
@@ -10,68 +10,81 @@ package com.desc.meetingbooker;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040008;
+        public static final int bluedark=0x7f040003;
+        public static final int bluelight=0x7f040002;
+        public static final int greendark=0x7f040005;
+        public static final int greenlight=0x7f040004;
+        public static final int grey=0x7f040001;
+        public static final int orangedark=0x7f040007;
+        public static final int orangelight=0x7f040006;
+        public static final int white=0x7f040000;
+    }
     public static final class drawable {
         public static final int action_settings=0x7f020000;
         public static final int borders=0x7f020001;
         public static final int content_discard=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int list_borders=0x7f020004;
-        public static final int pressed_color=0x7f020006;
-        public static final int relative_selector=0x7f020005;
+        public static final int image_pressed=0x7f020008;
+        public static final int imageview_selector=0x7f020004;
+        public static final int list_borders=0x7f020005;
+        public static final int pressed_color=0x7f020007;
+        public static final int relative_selector=0x7f020006;
     }
     public static final class id {
-        public static final int addButton=0x7f070021;
-        public static final int buttonLay=0x7f07000f;
-        public static final int calendarName=0x7f070003;
-        public static final int cancelButton=0x7f070022;
-        public static final int changePassword=0x7f070025;
-        public static final int changePrompt=0x7f070028;
-        public static final int clock=0x7f070004;
-        public static final int curnextLay=0x7f070007;
-        public static final int currentAvail=0x7f070005;
-        public static final int currentDesc=0x7f07000b;
-        public static final int currentEnd=0x7f07000e;
-        public static final int currentOrganizer=0x7f07000a;
-        public static final int currentStart=0x7f07000d;
-        public static final int currentTitle=0x7f070009;
-        public static final int currentUpcom=0x7f070008;
-        public static final int deleteButton=0x7f070017;
-        public static final int descTextView=0x7f070018;
-        public static final int editDesc=0x7f070019;
-        public static final int editTitle=0x7f070016;
-        public static final int endMeetingButton=0x7f070011;
-        public static final int intervalView=0x7f070020;
-        public static final int line=0x7f070006;
-        public static final int listView1=0x7f070012;
-        public static final int list_content=0x7f07002c;
-        public static final int mainLay=0x7f070000;
-        public static final int margain=0x7f070002;
-        public static final int menu_settings=0x7f070033;
-        public static final int newMeetingButton=0x7f070013;
-        public static final int nextMeetingButton=0x7f070010;
-        public static final int numberPicker=0x7f07002d;
-        public static final int pwEdit=0x7f07002f;
-        public static final int pwNew1=0x7f07002a;
-        public static final int pwNew2=0x7f07002b;
-        public static final int pwOld=0x7f070029;
-        public static final int pwPrompt=0x7f07002e;
-        public static final int settingCancel=0x7f070027;
-        public static final int settingCheck=0x7f070032;
-        public static final int settingList=0x7f070024;
-        public static final int settingName=0x7f070030;
-        public static final int settingSave=0x7f070026;
-        public static final int settingVal=0x7f070031;
-        public static final int settingsButton=0x7f070001;
-        public static final int startEndLay=0x7f07000c;
-        public static final int textEnd=0x7f07001e;
-        public static final int textStart=0x7f07001b;
-        public static final int timeEnd=0x7f07001d;
-        public static final int timePickerEnd=0x7f07001f;
-        public static final int timePickerStart=0x7f07001c;
-        public static final int timeStart=0x7f07001a;
-        public static final int titleLay=0x7f070015;
-        public static final int titleTextView=0x7f070014;
-        public static final int updateButton=0x7f070023;
+        public static final int addButton=0x7f080021;
+        public static final int buttonLay=0x7f08000f;
+        public static final int calendarName=0x7f080003;
+        public static final int cancelButton=0x7f080022;
+        public static final int changePassword=0x7f080025;
+        public static final int changePrompt=0x7f080028;
+        public static final int clock=0x7f080004;
+        public static final int curnextLay=0x7f080007;
+        public static final int currentAvail=0x7f080005;
+        public static final int currentDesc=0x7f08000b;
+        public static final int currentEnd=0x7f08000e;
+        public static final int currentOrganizer=0x7f08000a;
+        public static final int currentStart=0x7f08000d;
+        public static final int currentTitle=0x7f080009;
+        public static final int currentUpcom=0x7f080008;
+        public static final int deleteButton=0x7f080017;
+        public static final int descTextView=0x7f080018;
+        public static final int editDesc=0x7f080019;
+        public static final int editTitle=0x7f080016;
+        public static final int endMeetingButton=0x7f080011;
+        public static final int intervalView=0x7f080020;
+        public static final int line=0x7f080006;
+        public static final int listView1=0x7f080012;
+        public static final int list_content=0x7f08002c;
+        public static final int mainLay=0x7f080000;
+        public static final int margain=0x7f080002;
+        public static final int menu_settings=0x7f080033;
+        public static final int newMeetingButton=0x7f080013;
+        public static final int nextMeetingButton=0x7f080010;
+        public static final int numberPicker=0x7f08002d;
+        public static final int pwEdit=0x7f08002f;
+        public static final int pwNew1=0x7f08002a;
+        public static final int pwNew2=0x7f08002b;
+        public static final int pwOld=0x7f080029;
+        public static final int pwPrompt=0x7f08002e;
+        public static final int settingCancel=0x7f080027;
+        public static final int settingCheck=0x7f080032;
+        public static final int settingList=0x7f080024;
+        public static final int settingName=0x7f080030;
+        public static final int settingSave=0x7f080026;
+        public static final int settingVal=0x7f080031;
+        public static final int settingsButton=0x7f080001;
+        public static final int startEndLay=0x7f08000c;
+        public static final int textEnd=0x7f08001e;
+        public static final int textStart=0x7f08001b;
+        public static final int timeEnd=0x7f08001d;
+        public static final int timePickerEnd=0x7f08001f;
+        public static final int timePickerStart=0x7f08001c;
+        public static final int timeStart=0x7f08001a;
+        public static final int titleLay=0x7f080015;
+        public static final int titleTextView=0x7f080014;
+        public static final int updateButton=0x7f080023;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -84,39 +97,41 @@ public final class R {
         public static final int setting_item=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_edit=0x7f060000;
-        public static final int activity_main=0x7f060001;
-        public static final int activity_new_meeting=0x7f060002;
-        public static final int activity_settings=0x7f060003;
+        public static final int activity_edit=0x7f070000;
+        public static final int activity_main=0x7f070001;
+        public static final int activity_new_meeting=0x7f070002;
+        public static final int activity_settings=0x7f070003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f04000d;
-        public static final int menu_settings=0x7f040001;
-        public static final int text_add=0x7f040009;
-        public static final int text_cancel=0x7f040008;
-        public static final int text_change_password=0x7f040017;
-        public static final int text_confirm_password=0x7f040016;
-        public static final int text_delete=0x7f040019;
-        public static final int text_description=0x7f040004;
-        public static final int text_end=0x7f040007;
-        public static final int text_endmeeting=0x7f04000c;
-        public static final int text_extendEnd=0x7f040011;
-        public static final int text_new_password=0x7f040015;
-        public static final int text_newmeeting=0x7f04000a;
-        public static final int text_nextmeeting=0x7f04000b;
-        public static final int text_old_password=0x7f040014;
-        public static final int text_password=0x7f040013;
-        public static final int text_save=0x7f040012;
-        public static final int text_settings=0x7f040010;
-        public static final int text_spontaneous=0x7f040005;
-        public static final int text_start=0x7f040006;
-        public static final int text_title=0x7f040003;
-        public static final int text_update=0x7f04001a;
-        public static final int text_wrong_password=0x7f040018;
-        public static final int title_activity_edit=0x7f04000e;
-        public static final int title_activity_new_meeting=0x7f040002;
-        public static final int title_activity_settings=0x7f04000f;
+        public static final int app_name=0x7f050000;
+        public static final int delete_description=0x7f05001c;
+        public static final int hello_world=0x7f05000d;
+        public static final int menu_settings=0x7f050001;
+        public static final int settings_description=0x7f05001b;
+        public static final int text_add=0x7f050009;
+        public static final int text_cancel=0x7f050008;
+        public static final int text_change_password=0x7f050017;
+        public static final int text_confirm_password=0x7f050016;
+        public static final int text_delete=0x7f050019;
+        public static final int text_description=0x7f050004;
+        public static final int text_end=0x7f050007;
+        public static final int text_endmeeting=0x7f05000c;
+        public static final int text_extendEnd=0x7f050011;
+        public static final int text_new_password=0x7f050015;
+        public static final int text_newmeeting=0x7f05000a;
+        public static final int text_nextmeeting=0x7f05000b;
+        public static final int text_old_password=0x7f050014;
+        public static final int text_password=0x7f050013;
+        public static final int text_save=0x7f050012;
+        public static final int text_settings=0x7f050010;
+        public static final int text_spontaneous=0x7f050005;
+        public static final int text_start=0x7f050006;
+        public static final int text_title=0x7f050003;
+        public static final int text_update=0x7f05001a;
+        public static final int text_wrong_password=0x7f050018;
+        public static final int title_activity_edit=0x7f05000e;
+        public static final int title_activity_new_meeting=0x7f050002;
+        public static final int title_activity_settings=0x7f05000f;
     }
     public static final class style {
         /** 
@@ -140,11 +155,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
-        public static final int black_text=0x7f050002;
+        public static final int AppTheme=0x7f060001;
+        public static final int black_text=0x7f060002;
     }
 }
